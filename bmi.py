8d946f30e585e058fd801f4efa081e9c73af5224
Marcin Miksa <mmiksa@vp.pl>
2018-10-03 17:38:47 +0200

    #update2

--- bmi.py
+++ bmi.py
@@ -9,7 +9,7 @@ def bmi_counter():
         weight = float(weight_entry.get())
         bmi = (weight / height ** 2) * 100 * 100
         bmi = round(bmi, 2)
-        result = bmi_result.configure(text=bmi)
+        result = bmi_result.configure(text=bmi), results_list.insert(1, bmi)
     except ValueError:
         return None
     return result
@@ -17,7 +17,7 @@ def bmi_counter():
 
 top = Tk()
 top.title("BMI")
-top.geometry("450x250")
+top.geometry("440x420")
 top.configure(background="#808080")  # gray background
 
 height_label = Label(top, text="Wzrost")
@@ -47,12 +47,15 @@ bmi_button = Button(top, text="Oblicz BMI", command=bmi_counter)  # <-return res
 bmi_button.configure(height=5, width=10)
 bmi_button.place(x=190, y=120)
 
-results_list = Label(top, text="Lista wynikow")
-results_list.place(x=320, y=20)
+results_name = Label(top, text="Lista wynikow")
+results_name.place(x=320, y=20)
 
-results_list1 = Listbox(top)
-results_list1.place(x=320, y=50)
-results_list1.configure(height=5, width=7)
-results_list1.insert(1, '@sample')
+results_list = Listbox(top)
+results_list.place(x=320, y=50)
+results_list.configure(height=9, width=10)
+
+picture = LabelFrame(top)
+picture.configure(height=150, width=390, background="#9f2929")
+picture.place(x=20, y=220)
 
 top.mainloop()
